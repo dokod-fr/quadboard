c1c51d1222b99013e2790acc54034226143d9aa5
Aurélien <contact@dokod.fr>
2026-07-31 12:59:42 +0200

    feat(route): Add groups from session on /me

--- internal/http/handlers/me.go
+++ internal/http/handlers/me.go
@@ -18,18 +18,22 @@ func NewMeHandler(cfg *config.Config) *MeHandler {
 
 func (h *MeHandler) Serve(w http.ResponseWriter, r *http.Request) {
 	username := ""
+	groups := make([]string, 0)
 
 	if h.cfg.Auth.OIDC != nil && h.cfg.Auth.OIDC.Issuer != "" {
 		if session, ok := auth.SessionFromContext(r.Context()); ok {
 			username = session.Username
+			groups = session.Groups
 		}
 	}
 
 	response := struct {
-		Username       string `json:"username"`
-		GroupByDefault bool   `json:"groupByDefault"`
+		Username       string   `json:"username"`
+		Groups         []string `json:"groups"`
+		GroupByDefault bool     `json:"groupByDefault"`
 	}{
 		Username:       username,
+		Groups:         groups,
 		GroupByDefault: h.cfg.UI.GroupByDefault,
 	}
 
